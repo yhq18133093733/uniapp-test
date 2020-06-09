a8884ab3e82337deed6e8bea82a4e55640be1322
yhq <243790127@qq.com>
2020-06-10 00:09:43 +0800

    初始化

---
+++ components/test.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		test组件
+		<button type="warn">按钮</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
